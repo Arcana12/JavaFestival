3effaaee0b74c67f6b4a71b13c568e49424499fc
daeil jung <96384842+Arcana12@users.noreply.github.com>
2022-07-13 13:59:12 +0900

    DI

--- src/DI/ex11.java
+++ src/DI/ex11.java
@@ -7,7 +7,7 @@ public class ex11 {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("1.소프트웨어 설계 >> ");
-		int os = sc.nextInt();
+		int sw = sc.nextInt();
 		System.out.print("2.소프트웨어 개발 >> ");
 		int dev = sc.nextInt();
 		System.out.print("3.데이터베이스 구축 >> ");
@@ -17,7 +17,12 @@ public class ex11 {
 		System.out.print("5.정보시스템 구축관리 >> ");
 		int info = sc.nextInt();
 		
-		
+		if(sw>=8 && dev>=8 && db>=8 && pl>= 8 && info >=8 && sw+dev+db+pl+info>=60 ) {
+			System.out.println("합격입니다!");
+		}
+		else {
+			System.out.println("탈락입니다...");
+		}
 		
 		
 	}
